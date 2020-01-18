1a15c1c04ddf2a7688c87a76656f930586aa07be
TokyoParis <44323997+TokyoParis@users.noreply.github.com>
2020-01-18 10:42:16 -0600

    Update Core.java

--- src/main/Core.java
+++ src/main/Core.java
@@ -4,5 +4,6 @@ public class Core {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("good bye world");
+		System.out.println("this should only appear in my branch");
 	}
 }
